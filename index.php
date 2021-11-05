c192c3e98ac3fa184b00985b13f377606ef92f99
SABAREESWARANMN <90568983+SABAREESWARAN-M@users.noreply.github.com>
2021-11-05 18:03:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title></title>
 </head>
 <body>
-<form action="Associativearraygradecheckng.php" method="POST" align="center">
+<form action="index.php" method="POST" align="center">
 <h2>Student Result using user input through Associativearray</h2><br>
 	<label>Enter Student name( like Type "101" or "102")</label>
 	<input type="text" name="name"><br><br>
